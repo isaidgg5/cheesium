55035b7672b465b9e87155b14ae088d3d7623674
aj <134237782+ajtabjs@users.noreply.github.com>
2026-04-22 06:28:43 -0700

    add no referrer

--- ads.js
+++ ads.js
@@ -1,16 +1,11 @@
-
 const brokie = "https://recollectsideway.com/u51ydi5ybu?key=3b96cebdf6fcc47e6e2cf07f0034c04c"
-
 const key = "hasOpenedTab";
-
 document.addEventListener("DOMContentLoaded", () => {
   if (sessionStorage.getItem(key)) return;
-
   function firstclick() {
-    window.open(brokie, "_blank");
+    window.open(brokie, "_blank", "noopener,noreferrer");
     sessionStorage.setItem(key, "1");
     document.removeEventListener("click", firstclick);
   }
-
   document.addEventListener("click", firstclick);
 });
