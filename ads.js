8bd3b08a24aa25d19e0d4a78646b45717bf61489
aj <134237782+ajtabjs@users.noreply.github.com>
2026-04-22 04:24:30 +0000

    ads

---
+++ ads.js
@@ -0,0 +1,16 @@
+
+const brokie = "https://recollectsideway.com/u51ydi5ybu?key=3b96cebdf6fcc47e6e2cf07f0034c04c"
+
+const key = "hasOpenedTab";
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (sessionStorage.getItem(key)) return;
+
+  function firstclick() {
+    window.open(brokie, "_blank");
+    sessionStorage.setItem(key, "1");
+    document.removeEventListener("click", firstclick);
+  }
+
+  document.addEventListener("click", firstclick);
+});
